626f2bb2874551b0e400e1f46b028a13dcb8bf5d
Turbobot <turbobot@vercel.com>
2024-09-16 14:49:34 +0200

    feat(create-turbo): apply official-starter transform

--- packages/core/index.ts
+++ packages/core/index.ts
@@ -18,7 +18,7 @@ export async function createProject(options: ProjectOptions) {
   process.chdir('web');
   execSync(`pnpm up next@15.0.0-canary.156 react@rc react-dom@rc eslint-config-next@rc`, { stdio: 'inherit' });
   execSync(`pnpm dlx create-payload-app@beta`)
-  process.chdir('../../..');
+  process.chdir('../..');
   execSync(`pnpm install @supabase/supabase-js`, { stdio: 'inherit' });
 
   if (useInngest) {
