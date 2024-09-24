d856a2a87185b1d047068a927764b2500a3300e3
Turbobot <turbobot@vercel.com>
2024-09-24 18:56:23 +0200

    feat(create-turbo): apply official-starter transform

--- packages/core/utils/supabase/install.ts
+++ packages/core/utils/supabase/install.ts
@@ -10,7 +10,7 @@ export const installSupabase = (destinationDirectory: string) => {
   execSync(`supabase init`, { stdio: 'inherit' });
 
   // Correctly set the template directory using __dirname
-  const templateDirectory = path.join(__dirname, "../../templates");
+  const templateDirectory = path.join(__dirname, "../templates");
 
   templateGenerator(supabaseFiles, templateDirectory, destinationDirectory);
   console.log(`🍸 Supabase installed successfully!`)
