f074597fc71d4c82bb579fd6820f8bcf7ba1f2a3
Turbobot <turbobot@vercel.com>
2024-09-24 14:29:30 +0200

    feat(create-turbo): apply official-starter transform

--- packages/core/utils/payload/install.ts
+++ packages/core/utils/payload/install.ts
@@ -1,6 +1,6 @@
 import { execSync } from "child_process";
-import { existsSync } from "fs";
-import { join } from "path";
+import fs, { existsSync } from "fs";
+import path, { join } from "path";
 import { removeTurboFlag } from "../removeTurboFlag";
 
 const updatePackages = () => {
@@ -17,8 +17,50 @@ const updatePackages = () => {
   });
 };
 
+const prepareTsConfig = () => {
+  console.log("🍸 Preparing tsconfig.json...");
+
+  // Path to your tsconfig.json file
+  const tsconfigPath = path.join(process.cwd(), "tsconfig.json");
+
+  // Read the tsconfig.json file
+  fs.readFile(tsconfigPath, "utf8", (err, data) => {
+    if (err) {
+      console.error("🍸 Error reading tsconfig.json", err);
+      return;
+    }
+
+    // Parse the JSON data
+    const tsconfig = JSON.parse(data);
+
+    // Ensure compilerOptions exists
+    if (!tsconfig.compilerOptions) {
+      tsconfig.compilerOptions = {};
+    }
+
+    // Add the "paths" field to compilerOptions if it doesn't exist
+    if (!tsconfig.compilerOptions.paths) {
+      tsconfig.compilerOptions.paths = {};
+    }
+
+    // Append the "@payload-config" path
+    tsconfig.compilerOptions.paths["@payload-config"] = ["./payload.config.ts"];
+
+    // Write the updated tsconfig.json back to the file
+    fs.writeFile(tsconfigPath, JSON.stringify(tsconfig, null, 2), (err) => {
+      if (err) {
+        console.error("🍸 Error writing to tsconfig.json", err);
+      }
+    });
+  });
+};
+
 export const preparePayload = () => {
+  console.log("🍸 Initializing Payload...");
+  prepareTsConfig();
+
   process.chdir("./apps/web/");
+
   updatePackages();
 
   // Payload doesn't work with Turbopack yet
@@ -39,9 +81,7 @@ export const preparePayload = () => {
 
   // Check if the payload configuration file exists
   const payloadConfigPath = join(process.cwd(), "payload.config.ts");
-  if (existsSync(payloadConfigPath)) {
-    console.log("🍸 Payload installed successfully!");
-  } else {
+  if (!existsSync(payloadConfigPath)) {
     console.error("🍸 Payload installation cancelled/failed.");
   }
 };
