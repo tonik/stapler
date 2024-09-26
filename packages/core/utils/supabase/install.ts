d555c17acc9827c537ae02edb9a553aa852e58a3
Turbobot <turbobot@vercel.com>
2024-09-26 12:33:23 +0200

    feat(create-turbo): apply official-starter transform

--- packages/core/utils/supabase/install.ts
+++ packages/core/utils/supabase/install.ts
@@ -1,17 +1,17 @@
 import { execSync } from "child_process";
 import { templateGenerator } from "../generator/generator";
 import { supabaseFiles } from "../../templates/supabase/installConfig";
-import path from 'path';
+import path from "path";
 
 export const installSupabase = (destinationDirectory: string) => {
-  console.log("🍸 Installing supabase-js...");
-  execSync(`pnpm install @supabase/supabase-js`, { stdio: 'inherit' });
-  console.log('🍸 Adding Supabase Files...');
-  execSync(`supabase init`, { stdio: 'inherit' });
+  console.log("🍸 Installing supabase-js...", process.cwd());
+  execSync(`pnpm install @supabase/supabase-js`, { stdio: "inherit" });
+  console.log("🍸 Adding Supabase Files...");
+  execSync(`supabase init`, { stdio: "inherit" });
 
   // Correctly set the template directory using __dirname
   const templateDirectory = path.join(__dirname, "../templates");
 
   templateGenerator(supabaseFiles, templateDirectory, destinationDirectory);
-  console.log(`🍸 Supabase installed successfully!`)
+  console.log(`🍸 Supabase installed successfully!`);
 };
