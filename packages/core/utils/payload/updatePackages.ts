fae00f5764af2c78fccbfd729786cc751a5e2065
Turbobot <turbobot@vercel.com>
2024-09-26 16:46:18 +0200

    feat(create-turbo): apply official-starter transform

--- packages/core/utils/payload/updatePackages.ts
+++ packages/core/utils/payload/updatePackages.ts
@@ -1,15 +1,12 @@
 import { execSync } from "child_process";
 
 export const updatePackages = () => {
-  console.log(
-    "🍸 Updating Next and React to their respective release candidates..."
-  );
-  execSync(`pnpm up next@rc react@rc react-dom@rc eslint-config-next@rc`, {
-    stdio: "inherit",
-  });
-
   console.log("🍸 Installing necessary packages...");
-  execSync(`pnpm up pg sharp`, {
-    stdio: "inherit",
-  });
+
+  execSync(
+    `pnpm up next@rc react@rc react-dom@rc eslint-config-next@rc pg sharp`,
+    {
+      stdio: "inherit",
+    }
+  );
 };
