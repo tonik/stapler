f3ed90bbaa370336cbac8ccaa973243a59f9bdb6
Karolina Kopacz <karolina.kopacz94@gmail.com>
2024-10-17 16:41:30 +0200

    feat: get vercel deployment urls

--- packages/core/utils/vercel/index.ts
+++ packages/core/utils/vercel/index.ts
@@ -1,20 +1,26 @@
+import { addEnvironmentVariable } from './addEnvVariables';
+import { connectToGit } from './connect';
 import { getDeploymentUrl } from './getDeploymentUrl';
+import { initializeVercelProject } from './initialize';
+import { installVercel } from './install';
+import { linkVercelProject } from './link';
+import { loginToVercel } from './login';
 
 export async function setupVercel() {
-  // installVercel();
-  // loginToVercel();
-  // initializeVercelProject();
-  // linkVercelProject();
+  installVercel();
+  loginToVercel();
+  initializeVercelProject();
+  linkVercelProject();
 
-  // const supabaseUrl = process.env.SUPABASE_URL;
-  // const supabaseAnonKey = process.env.SUPABASE_ANON_KEY;
+  const supabaseUrl = process.env.SUPABASE_URL;
+  const supabaseAnonKey = process.env.SUPABASE_ANON_KEY;
 
-  // if (supabaseUrl) {
-  //   addEnvironmentVariable({ name: 'SUPABASE_URL', value: supabaseUrl, deployment: 'production' });
-  // }
-  // if (supabaseAnonKey) {
-  //   addEnvironmentVariable({ name: 'SUPABASE_ANON_KEY', value: supabaseAnonKey, deployment: 'production' });
-  // }
+  if (supabaseUrl) {
+    addEnvironmentVariable({ name: 'SUPABASE_URL', value: supabaseUrl, deployment: 'production' });
+  }
+  if (supabaseAnonKey) {
+    addEnvironmentVariable({ name: 'SUPABASE_ANON_KEY', value: supabaseAnonKey, deployment: 'production' });
+  }
 
   console.log('🖇️  Creating preview deployment...');
   const previewUrl = getDeploymentUrl(false);
@@ -24,5 +30,5 @@ export async function setupVercel() {
 
   console.log('____', previewUrl, productionUrl);
 
-  // connectToGit();
+  connectToGit();
 }
