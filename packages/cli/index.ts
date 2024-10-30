3b00fe655e6c229511a3aab2addfec8f71fb0bd6
Przemyslaw Mejna <pmejna@gmail.com>
2024-10-30 17:28:08 +0100

    fix(cli): remove the second argument from createProject funct call

--- packages/cli/index.ts
+++ packages/cli/index.ts
@@ -84,7 +84,7 @@ const createAction = async () => {
   if (!proceedWithNewProject && selectedProject) {
     process.chdir(selectedProject.projectPath);
     selectedProject.state.options.name = selectedProject.projectName;
-    await createProject(selectedProject.state.options, selectedProject.projectPath)
+    await createProject(selectedProject.state.options)
       .then(() => {
         console.log('Project resumed successfully!');
       })
@@ -135,7 +135,7 @@ const createAction = async () => {
 
     const finalOptions = { ...answers, ...payloadAnswer };
 
-    await createProject(finalOptions, projectDir)
+    await createProject(finalOptions)
       .then(() => {
         console.log(chalk.green('Project created successfully!'));
         console.log(chalk.green(`with options: ${JSON.stringify(finalOptions)}`));
