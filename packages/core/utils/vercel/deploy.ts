5c5fda5c788c06d2105a155b82fc723649144c4b
Martin Chełminiak <mc@tonik.com>
2024-11-04 12:19:58 +0100

    refactor: remove getDeploymentURL function

--- packages/core/utils/vercel/deploy.ts
+++ packages/core/utils/vercel/deploy.ts
@@ -1,6 +1,6 @@
 import fs from 'fs/promises';
+import { execSync } from 'node:child_process';
 import { connectWithGH } from './connectWithGH';
-import { getDeploymentUrl } from './utils/getDeploymentUrl';
 import { getLogColor } from '../shared/getLogColor';
 
 export const deployVercelProject = async () => {
@@ -20,7 +20,16 @@ export const deployVercelProject = async () => {
   await fs.writeFile('vercel.json', JSON.stringify(vercelConfig, null, 2));
 
   getLogColor('vercel', 'Creating production deployment...');
-  const productionUrl = getDeploymentUrl();
 
-  getLogColor('vercel', `You can access your production deployment at: \x1b[36m${productionUrl}\x1b[0m`);
+  const productionUrl = execSync('vercel --prod', {
+    stdio: ['inherit', 'pipe', 'inherit'],
+    encoding: 'utf8',
+  });
+
+  if (productionUrl) {
+    getLogColor('vercel', `You can access your production deployment at: \x1b[36m${productionUrl}\x1b[0m`);
+  } else {
+    getLogColor('vercel', 'Failed to create production deployment.');
+    return;
+  }
 };
