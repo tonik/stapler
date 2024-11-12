f9c5684bf4e4ffc2403cdba361ced74345b3a321
Martin Chełminiak <mc@tonik.com>
2024-11-12 14:42:54 +0100

    refactor: extract loginIfNeeded in vercel setup

--- packages/core/installMachine/installSteps/vercel/setupAndCreate.ts
+++ packages/core/installMachine/installSteps/vercel/setupAndCreate.ts
@@ -1,47 +1,49 @@
 import { execSync } from 'child_process';
-import { logWithColoredPrefix } from '../../../utils/logWithColoredPrefix';
 import chalk from 'chalk';
+import { logWithColoredPrefix } from '../../../utils/logWithColoredPrefix';
+import { updateProjectSettings } from './updateProjectSettings';
 
-const getUserName = (): string | null => {
+const getUsername = (): string | null => {
   try {
-    const user = execSync('npx vercel whoami', { stdio: 'pipe', encoding: 'utf-8' });
-    return user;
+    const user = execSync('npx vercel whoami', { stdio: 'pipe', encoding: 'utf-8' }).trim();
+    return user || null;
   } catch {
     return null;
   }
 };
 
-export const setupAndCreateVercelProject = async () => {
-  const vercelUserName = getUserName();
-
-  if (!vercelUserName) {
-    logWithColoredPrefix('vercel', 'Logging in...');
+const loginIfNeeded = () => {
+  logWithColoredPrefix('vercel', 'Logging in...');
+  try {
+    execSync('npx vercel login', { stdio: 'inherit' });
+  } catch (error) {
+    logWithColoredPrefix('vercel', [
+      'Oops! Something went wrong while logging in...',
+      '\nYou might already be logged in with this email in another project.',
+      '\nIn this case, select "Continue with Email" and enter the email you\'re already logged in with.\n',
+    ]);
     try {
       execSync('npx vercel login', { stdio: 'inherit' });
-    } catch (error) {
+    } catch {
       logWithColoredPrefix('vercel', [
-        'Oops! Something went wrong while logging in...',
-        '\nYou might already be logged in with this email in another project.',
-        '\nIn this case, select "Continue with Email" and enter the email you\'re already logged in with.\n',
+        'Please check the error above and try again.',
+        '\nAfter successfully logging in with "vercel login", please run create-stapler-app again.\n',
       ]);
-      try {
-        execSync('npx vercel login', { stdio: 'inherit' });
-      } catch {
-        logWithColoredPrefix('vercel', [
-          'Please check the error above and try again.',
-          '\nAfter successfully logging in with "vercel login", please run create-stapler-app again.\n',
-        ]),
-          process.exit(1);
-      }
+      process.exit(1);
     }
-  } else {
-    logWithColoredPrefix('vercel', `You are logged in as \x1b[36m${vercelUserName.toString().trim()}\x1b[0m`);
+  }
+};
+
+export const setupAndCreateProject = async () => {
+  let vercelUserName = getUsername();
+
+  if (!vercelUserName) {
+    loginIfNeeded();
+    vercelUserName = getUsername(); // Retry getting username after login
   }
 
+  logWithColoredPrefix('vercel', `You are logged in as ${chalk.cyan(vercelUserName)}`);
+
   logWithColoredPrefix('vercel', 'Linking project...');
-  logWithColoredPrefix(
-    'vercel',
-    `NOTE: You need to specify manually ${chalk.cyan('in which directory is your code located')}, should be: ${chalk.greenBright('./apps/web')}`,
-  );
-  execSync('npx vercel link', { stdio: 'inherit' });
+  execSync('npx vercel link --yes', { stdio: 'ignore' });
 };
