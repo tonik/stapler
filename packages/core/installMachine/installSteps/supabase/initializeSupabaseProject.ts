b2105ac676de890e690b8599172f56e004c9b94c
Martin Chełminiak <mc@tonik.com>
2024-11-14 16:54:38 +0100

    refactor: extract functions to files

---
+++ packages/core/installMachine/installSteps/supabase/initializeSupabaseProject.ts
@@ -0,0 +1,22 @@
+import { execAsync } from "../../../utils/execAsync";
+import { logger } from "../../../utils/logger";
+
+export const initializeSupabaseProject = async () => {
+  await logger.withSpinner('supabase', 'Initializing project...', async (spinner) => {
+    try {
+      await execAsync(`npx supabase init`);
+      spinner.succeed('Project initialized.');
+    } catch (error: any) {
+      const errorMessage = error.stderr;
+      if (errorMessage.includes('file exists')) {
+        spinner.succeed('Configuration file already exists.');
+      } else {
+        spinner.fail('Failed to initialize project.');
+        console.error(
+          'Please review the error message below, follow the initialization instructions, and try running "create-stapler-app" again.',
+        );
+        process.exit(1);
+      }
+    }
+  });
+};
