26497bd53678093f68849a5f8bc4ecf19aec3c82
Martin Chełminiak <mc@tonik.com>
2024-11-19 18:49:15 +0100

    feat: add pnpm version check

--- packages/core/installMachine/installSteps/turbo/create.ts
+++ packages/core/installMachine/installSteps/turbo/create.ts
@@ -1,9 +1,25 @@
 import { execAsync } from '../../../utils/execAsync';
 import { logger } from '../../../utils/logger';
 
+const checkPnpmVersion = async () => {
+  await logger.withSpinner('turborepo', 'Checking pnpm version...', async (spinner) => {
+    try {
+      // Run the command to check the pnpm version
+      const { stdout } = await execAsync('pnpm --version');
+      spinner.succeed(`Found pnpm version ${stdout.trim()}.`);
+    } catch (error) {
+      spinner.fail('Failed to find pnpm version. Visit https://pnpm.io/installation to install pnpm.');
+      console.error(error);
+    }
+  });
+};
+
 export const createTurboRepo = async (name: string) => {
   await logger.withSpinner('turborepo', 'Initializing...', async (spinner) => {
     try {
+      // Check the pnpm version
+      await checkPnpmVersion();
+      // Run the command to create a turbo repo
       await execAsync(`npx create-turbo@latest ${name} -m pnpm`);
       spinner.succeed('Initialized!');
     } catch (error) {
