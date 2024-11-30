7d12789dfbd156038b3bc2e344a5167f17474282
Martin Chełminiak <mc@tonik.com>
2024-11-30 15:22:59 +0100

    fix: tailwind installconfig

--- packages/core/templates/tailwind/installConfig.ts
+++ packages/core/templates/tailwind/installConfig.ts
@@ -1,6 +1,6 @@
 export const tailwindFiles = [
   {
     path: 'apps/web/',
-    files: ['postcss.config.js', 'tailwind.config.ts'],
+    files: ['postcss.config.cjs', 'tailwind.config.ts'],
   },
 ];
