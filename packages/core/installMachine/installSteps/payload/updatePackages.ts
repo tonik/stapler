5b449b1bb73b61d914d977761235bfa37a1a688d
Martin Chełminiak <mc@tonik.com>
2024-11-30 15:12:52 +0100

    chore: remove next update to @rc

--- packages/core/installMachine/installSteps/payload/updatePackages.ts
+++ packages/core/installMachine/installSteps/payload/updatePackages.ts
@@ -2,19 +2,15 @@ import { logger } from '../../../utils/logger';
 import { execAsync } from '../../../utils/execAsync';
 
 export const updatePackages = async () => {
-  await logger.withSpinner(
-    'payload',
-    'Updating Next and React to their respective release candidates...',
-    async (spinner) => {
-      try {
-        await execAsync(`pnpm up next@rc react@rc react-dom@rc eslint-config-next@rc --reporter silent`);
-        spinner.succeed('Updated Next and React to their respective release candidates.');
-      } catch (error) {
-        spinner.fail('Failed to update Next and React to their respective release candidates.');
-        console.error(error);
-      }
-    },
-  );
+  await logger.withSpinner('payload', `Updating React to it's respective release candidate...`, async (spinner) => {
+    try {
+      await execAsync(`pnpm up react@rc react-dom@rc eslint-config-next@rc --reporter silent`);
+      spinner.succeed(`Updated React to it's respective release candidate.`);
+    } catch (error) {
+      spinner.fail(`Failed to update React to it's respective release candidate.`);
+      console.error(error);
+    }
+  });
 
   await logger.withSpinner('payload', 'Installing necessary packages...', async (spinner) => {
     try {
