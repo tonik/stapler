845f26b563d3024dfb19f92329c947a7544a2cb6
Martin Chełminiak <mc@tonik.com>
2024-12-06 13:21:14 +0100

    fix: impossible to login to github

--- packages/core/installMachine/installSteps/github/repositoryManager.ts
+++ packages/core/installMachine/installSteps/github/repositoryManager.ts
@@ -1,4 +1,4 @@
-import { execSync } from 'child_process';
+import { execSync, spawnSync } from 'child_process';
 import inquirer from 'inquirer';
 import chalk from 'chalk';
 import { logger } from 'stplr-utils';
@@ -40,17 +40,26 @@ export const isGitHubAuthenticated = (): boolean => {
 export const authenticateGitHub = async () => {
   await logger.withSpinner('github', 'Attempting to authenticate...', async (spinner) => {
     try {
-      spinner.stop();
-
-      execSync('gh auth login', { stdio: 'inherit' });
-
       spinner.start('Authenticating...');
       const isAuthenticated = isGitHubAuthenticated();
+
       if (isAuthenticated) {
         spinner.succeed('Logged in');
         return true;
-      } else {
-        spinner.fail('Authentication failed.');
+      }
+
+      spinner.stop();
+
+      try {
+        const result = spawnSync('gh', ['auth', 'login'], {
+          stdio: 'inherit',
+          shell: true,
+        });
+        spinner.succeed('Logged in');
+        return true;
+      } catch (error) {
+        spinner.fail('Logging in failed.');
+        console.error('Logging in failed:', error);
         return false;
       }
     } catch (error) {
